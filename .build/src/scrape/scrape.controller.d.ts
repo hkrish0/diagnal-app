c628d65701a62ade627abc2a87fdd06c95fc8638
Harikrishnan <hkrish0@gmail.com>
2022-07-23 16:10:10 +0530

    updated READ ME file

--- .build/src/scrape/scrape.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express';
-import { ScrapeService } from './scrape.service';
-export declare class ScrapeController {
-    private readonly scrapeService;
-    constructor(scrapeService: ScrapeService);
-    processUrl(post: Request): any;
-}
--- .build/src/scrape/scrape.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Cache } from 'cache-manager';
-import { Request } from 'express';
-export declare class ScrapeService {
-    private cacheManager;
-    constructor(cacheManager: Cache);
-    create(req: Request): any;
-}
